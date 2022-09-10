ae9c53b33a955c2d18d720cf503afcb55ee5525f
alexwmw <alexwmw@gmail.com>
2022-09-10 11:37:16 +0100

    Info outline

--- src/content/information.js
+++ src/content/information.js
@@ -1,10 +1,28 @@
 const information = [
-  `### Heading
-  Text goes here`,
-  `### Heading
-  Text goes here`,
-  `### Heading
-  Text goes here`,
+  `
+  ### The 'go-to' function
+  Text goes here
+  `,
+  `
+  ### Adding and editing search providers
+  Text goes here
+  #### Provider name
+  Text goes here
+  #### Hostname
+  Text goes here
+  #### Query path
+  Text goes here
+  #### Favicon URL (optional)
+  Text goes here
+  `,
+  `
+  ### Opening a search in the background
+  Text goes here
+  `,
+  `
+  ### Examples
+  Text goes here
+  `,
 ];
 
 export default information;
