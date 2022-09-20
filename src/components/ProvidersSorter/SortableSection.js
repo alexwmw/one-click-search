968a9234d0354f2190ed41555689d4905b2b9d35
alex_wright <alex.wright@kallidus.com>
2022-09-20 22:05:35 +0100

    Reinstate limit on max n visible

--- src/components/ProvidersSorter/SortableSection.js
+++ src/components/ProvidersSorter/SortableSection.js
@@ -1,6 +1,5 @@
 import clsx from "clsx";
-import { useEffect } from "react";
-import { useState } from "react";
+
 import { ReactSortable } from "react-sortablejs";
 import SortableItem from "./SortableItem";
 import "./SortableSection.less";
@@ -8,23 +7,31 @@ import "./SortableSection.less";
 function SortableSection(props) {
   const { name, id, maxLength, list, setList, openItem, setOpenItem } = props;
 
-  // /** Visible list must be at most {maxLength} items */
-  // const putHandler = (to, from) => {
-  //   if (id == "visible") {
-  //     return to.el.children.length < maxLength;
-  //   }
-  //   return true;
-  // };
+  /** Visible list must be at most {maxLength} items */
+  const putHandler = (to, from) => {
+    if (!id === "visible") {
+      return true;
+    }
+    return to.el.children.length < maxLength;
+  };
 
   return (
-    <div className={clsx("sortable-section", list.length == 0 && "empty")}>
-      <h3 className="section-name">{`${name} search icons`}</h3>
+    <div
+      className={clsx(
+        "sortable-section",
+        list.length == 0 && "empty",
+        list.length === maxLength && "full"
+      )}
+    >
+      <h3 className="section-name">
+        {`${name} search icons`}
+        {maxLength && ` (max. ${maxLength})`}
+      </h3>
       <ReactSortable
         id={id}
         list={list}
         setList={setList}
-        // group={{ name: "iconsList", put: putHandler }}
-        group={{ name: "iconsList" }}
+        group={{ name: "iconsList", put: putHandler }}
         tag={"ul"}
         animation={150}
         filter={".undraggable"}
