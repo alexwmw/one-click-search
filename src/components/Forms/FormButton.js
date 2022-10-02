18eab61ba5df7986699ec678d2f0d340f0a5b293
alexwmw <alexwmw@gmail.com>
2022-10-02 16:33:20 +0100

    Fix bug

--- src/components/Forms/FormButton.js
+++ src/components/Forms/FormButton.js
@@ -1,32 +1,34 @@
+import clsx from "clsx";
 import Button from "../Buttons/Button";
 
 const FormButton = (props) => {
   const { type, onClick, label, classes = [] } = props;
 
-  const btnProps = {
-    submit: {
-      label: label || "Submit",
-      type: "submit",
-      icon: "save",
-      classes: [...classes, "submitBtn"],
-    },
-    delete: {
-      label: label || "Delete",
-      type: "button",
-      icon: "delete",
-      classes: [...classes, "deleteBtn"],
-    },
-    close: {
-      label: label || "Close",
-      type: "button",
-      icon: "close",
-      classes: [...classes, "closeBtn"],
-    },
-  }[type];
+  const btnProps =
+    {
+      submit: {
+        label: label || "Submit",
+        type: "submit",
+        icon: "save",
+        classes: [...classes, "submitBtn"],
+      },
+      delete: {
+        label: label || "Delete",
+        type: "button",
+        icon: "delete",
+        classes: [...classes, "deleteBtn"],
+      },
+      close: {
+        label: label || "Close",
+        type: "button",
+        icon: "close",
+        classes: [...classes, "closeBtn"],
+      },
+    }[type] ?? {};
 
   return (
     <Button
-      classes={["btn", "formBtn", btnProps.classes]}
+      classes={clsx("btn", "formBtn", btnProps.classes)}
       type={btnProps.type}
       onClick={onClick}
       icon={btnProps.icon}
